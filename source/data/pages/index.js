d206dadb02cb420c73a14397dd68d254c98b166f
Денис <Allimp_94@mail.ru>
2022-12-08 22:19:18 +0300

    fix

--- source/data/pages/index.js
+++ source/data/pages/index.js
@@ -13,4 +13,20 @@ export default {
       webpTablet2x: 'img/background-header-tablet@2x.webp',
     },
   },
+  about: {
+    content: `<h2>Путешествие на край Земли</h2>
+      <p>Это волшебное путешествие поможет вам познакомиться поближе с удивительным миром Антарктики.
+      Наш круизный лайнер отвезет вас на край Земли, где вы сможете увидеть: ледяные айсберги,
+      несколько видов пингвинов, китов, морских котиков и многое другое.</p>`,
+    image: {
+      default: 'img/about.jpg',
+      default2x: 'img/about@2x.jpg',
+      webp: 'img/about.webp',
+      webp2x: 'img/about@2x.webp',
+      webpMobile: 'img/about-mobile.webp',
+      webpMobile2x: 'img/about-mobile@2x.webp',
+      webpTablet: 'img/about-tablet.webp',
+      webpTablet2x: 'img/about-tablet@2x.webp',
+    },
+  },
 };
