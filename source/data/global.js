12ae582400e7fa5ba0e135b4d9a35163a36bd752
Денис <Allimp_94@mail.ru>
2022-12-08 19:42:51 +0300

    page-header

--- source/data/global.js
+++ source/data/global.js
@@ -1,1 +1,39 @@
-export default {};
+export default {
+  description: 'Сайт по организации туров в Антарктику.',
+  projectName: 'Антарктика',
+  navLinks: [
+    {
+      url: '#advantages',
+      title: 'Преимущества',
+    },
+    {
+      url: '#upcoming',
+      title: 'Направления',
+    },
+    {
+      url: '#upcoming',
+      title: 'Ближайшие круизы',
+    },
+    {
+      url: '#contacts',
+      title: 'Контакты',
+    },
+  ],
+  socials: [
+    {
+      id: 'ok',
+      title: 'Мы в Одноклассниках.',
+      url: '#!',
+    },
+    {
+      id: 'dzen',
+      title: 'Мы в Яндекс.Дзен',
+      url: '#!',
+    },
+    {
+      id: 'reddit',
+      title: 'Мы в Reddit',
+      url: '#!',
+    },
+  ],
+};
--- source/data/pages/index.js
+++ source/data/pages/index.js
@@ -1,1 +1,16 @@
-export default {};
+export default {
+  promo: {
+    content: `<h1>Круизы в Антарктику</h1>
+      <p>Волшебный мир айсбергов и пингвинов</p>`,
+    image: {
+      default: 'img/background-header.jpg',
+      default2x: 'img/background-header@2x.jpg',
+      webp: 'img/background-header.webp',
+      webp2x: 'img/background-header@2x.webp',
+      webpMobile: 'img/background-header-mobile.webp',
+      webpMobile2x: 'img/background-header-mobile@2x.webp',
+      webpTablet: 'img/background-header-tablet.webp',
+      webpTablet2x: 'img/background-header-tablet@2x.webp',
+    },
+  },
+};
