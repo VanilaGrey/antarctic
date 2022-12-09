139f90195616ac6a605f650b863a585fd6e8be79
Денис <Allimp_94@mail.ru>
2022-12-09 19:58:30 +0300

    about

--- source/data/pages/index.js
+++ source/data/pages/index.js
@@ -1,32 +1,28 @@
+const getImages = (filename, additions = {}) =>
+  Object.assign(additions, {
+    default: `img/${filename}.jpg`,
+    default2x: `img/${filename}@2x.jpg`,
+    webp: `img/${filename}.webp`,
+    webp2x: `img/${filename}@2x.webp`,
+    webpMobile: `img/${filename}-mobile.webp`,
+    webpMobile2x: `img/${filename}-mobile@2x.webp`,
+    webpTablet: `img/${filename}-tablet.webp`,
+    webpTablet2x: `img/${filename}-tablet@2x.webp`,
+  });
+
 export default {
   promo: {
     content: `<h1>Круизы в Антарктику</h1>
       <p>Волшебный мир айсбергов и пингвинов</p>`,
-    image: {
-      default: 'img/background-header.jpg',
-      default2x: 'img/background-header@2x.jpg',
-      webp: 'img/background-header.webp',
-      webp2x: 'img/background-header@2x.webp',
-      webpMobile: 'img/background-header-mobile.webp',
-      webpMobile2x: 'img/background-header-mobile@2x.webp',
-      webpTablet: 'img/background-header-tablet.webp',
-      webpTablet2x: 'img/background-header-tablet@2x.webp',
-    },
+    image: getImages('background-header'),
   },
   about: {
     content: `<h2>Путешествие на край Земли</h2>
       <p>Это волшебное путешествие поможет вам познакомиться поближе с удивительным миром Антарктики.
       Наш круизный лайнер отвезет вас на край Земли, где вы сможете увидеть: ледяные айсберги,
       несколько видов пингвинов, китов, морских котиков и многое другое.</p>`,
-    image: {
-      default: 'img/about.jpg',
-      default2x: 'img/about@2x.jpg',
-      webp: 'img/about.webp',
-      webp2x: 'img/about@2x.webp',
-      webpMobile: 'img/about-mobile.webp',
-      webpMobile2x: 'img/about-mobile@2x.webp',
-      webpTablet: 'img/about-tablet.webp',
-      webpTablet2x: 'img/about-tablet@2x.webp',
-    },
+    image: getImages('about', {
+      alt: 'Пингвины на снегу.',
+    }),
   },
 };
