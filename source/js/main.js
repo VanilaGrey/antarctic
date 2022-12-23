e461120d23746ab87303f7edad66cc7ad255c122
Денис <Allimp_94@mail.ru>
2022-12-23 15:55:14 +0300

    fix SEO

--- source/js/main.js
+++ source/js/main.js
@@ -1,14 +1,36 @@
-import initPageHeader from './modules/page-header.js';
 import initForm from './modules/form.js';
 import initMaps from './modules/map.js';
+import initPageHeader from './modules/page-header.js';
 
 document.querySelectorAll('.page-header').forEach(initPageHeader);
 
 // в load следует добавить скрипты, не участвующие в работе первого экрана
 window.addEventListener('load', () => {
-  document.querySelectorAll('[data-form]').forEach(initForm);
+  const initLazy = () => {
+    if (window.pageYOffset <= window.innerHeight) {
+      return;
+    }
 
-  initMaps(document.querySelectorAll('[data-map][id]'));
+    initMaps(document.querySelectorAll('[data-map][id]'));
+    document.querySelectorAll('[data-form]').forEach(initForm);
+
+    document.querySelectorAll('[data-lazy-style]').forEach((lazyStyledElement) => {
+      lazyStyledElement.setAttribute('style', lazyStyledElement.dataset.lazyStyle);
+      lazyStyledElement.removeAttribute('data-lazy-style');
+    });
+
+    document.removeEventListener('scroll', scrollHandler);
+  };
+
+  function scrollHandler() {
+    initLazy();
+  }
+
+  if (window.pageYOffset > window.innerHeight) {
+    initLazy();
+  } else {
+    document.addEventListener('scroll', scrollHandler);
+  }
 });
 
 // привязывайте js не на классы, а на дата-атрибуты (data-validate)
